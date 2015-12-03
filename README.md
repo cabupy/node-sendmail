baaab08fa623fc9dd17d159e7b2122b65bc9f374
Carlos Vallejos <carlos@vamyal.com>
2015-12-03 18:25:47 -0300

    Primera prueba de envio de mail con servidor propio, puerto 587 031215 18:25

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
 # node-sendmail
 
-Una prueba de un sistema para enviar correos electronicos con node.
+Generamos un proyecto como ejemplo para enviar correos desde nuestro propio servidor, usando el puerto 587.
+
+## Clonar el repo
+
+``` sh
+$ git clone https://github.com/cabupy/node-sendmail
+$ npm install
+```
+
+## Modulos requeridos
+- standard: usamos para escribir nuestro codigo de manera estandard.
+- nodemailer: es una libreria para el envio de correo electronico con node.js
+
+## Variables de entorno
+
+- VAMYAL_ SMTP__HOST
+- VAMYAL_SMTP_USER
+- VAMYAL_SMTP_PASS
+
+## Para ejecutar el server y probar el envio
+``` sh
+$ env VAMYAL_SMTP_HOST='host.dominio.com' VAMYAL_SMTP_USER='usuario@dominio.com' VAMYAL_SMTP_PASS='password' node server.js
+```
+
+
+## Licencia
+
+MIT
--- server.js
+++ server.js
@@ -1,1 +1,37 @@
-'use strict';
+/*
+    Empresa: Vamyal S.A.
+    Author: Carlos Vallejos
+    Fecha: Noviembre del 2015
+*/
+
+'use strict'
+
+var nodemailer = require('nodemailer')
+
+var transporter = nodemailer.createTransport({
+  host: process.env.VAMYAL_SMTP_HOST,
+  secureConnection: true,
+  port: 587,
+  auth: {
+    user: process.env.VAMYAL_SMTP_USER,
+    pass: process.env.VAMYAL_SMTP_PASS
+  },
+  tls: {rejectUnauthorized: false},
+  debug: true
+})
+
+var mailOptions = {
+  from: 'Carlos Vallejos <carlos@vamyal.com>',
+  to: 'carlos@vamyal.com',
+  subject: 'Asunto del mail',
+  // text: 'Cuerpo del mail en texto plano'
+  html: '<b>Hola Mundo desde nodemailer !</b>'
+}
+
+transporter.sendMail(mailOptions, function (error, info) {
+  if (error) {
+    return console.log(error)
+  }
+  console.log('Message sent: ' + info.response)
+})
+
